fb49ced6be2cfc92f01f2aa2365023a6f8e8959e
Willian <willian.alves.b15@gmail.com>
2023-01-25 16:29:06 -0300

    feat: created array of icons in utils

---
+++ src/utils/index.ts
@@ -0,0 +1,46 @@
+import react from "../assets/react.svg";
+import next from "../assets/next.svg";
+import ts from "../assets/typescript.svg";
+import js from "../assets/javascript.svg";
+import styled from "../assets/styled.svg";
+import html from "../assets/html.svg";
+import css3 from "../assets/css3.svg";
+
+
+export const iconsTec = [
+  {
+    id: 1,
+    img: react,
+    name: "ReactJS",
+  },
+  {
+    id: 2,
+    img: next,
+    name: "NextJS",
+  },
+  {
+    id: 3,
+    img: ts,
+    name: "TypeScript",
+  },
+  {
+    id: 4,
+    img: js,
+    name: "JavaScript",
+  },
+  {
+    id: 5,
+    img: styled,
+    name: "Styled-Components",
+  },
+  {
+    id: 6,
+    img: html,
+    name: "HTML5",
+  },
+  {
+    id: 7,
+    img: css3,
+    name: "CSS3",
+  },
+];
