eec787673b384e328febc52e42b761a7ff18aeb4
Willian <willian.alves.b15@gmail.com>
2023-03-06 15:57:15 -0300

    styles: header

--- src/components/header/header.style.ts
+++ src/components/header/header.style.ts
@@ -3,4 +3,36 @@ import styled from "styled-components";
 
 
 export const Header = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 119px;
+  position: fixed;
+  width: -webkit-fill-available;
+
+
+  ul {
+    list-style-type: none;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 514px;
+    height: 39px;
+    background-color: rgba(255, 255, 255, 0.401);
+    padding: 10px 20px;
+    border: none;
+    border-radius: 40px;
+  }
+
+  a {
+    display: inline-block;
+    margin: 0 15px;
+    color: #ffffff;
+    text-decoration: none;
+  }
+
+  a:hover  {
+    color: #5ad5e4;
+    transition: 0.7s;
+  }
 `;
