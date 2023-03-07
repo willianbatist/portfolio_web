7449247b3ec4a44816c3a4d7ab904a1faa23899e
Willian <willian.alves.b15@gmail.com>
2023-03-06 21:08:34 -0300

    styles(Title): h1, span

--- src/components/title/title.style.ts
+++ src/components/title/title.style.ts
@@ -3,11 +3,35 @@ import styled from "styled-components";
 
 
 export const Title = styled.section`
-  position: relative;
   min-height: 100vh;
   border: 2px solid yellow;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  h1 {
+    text-transform: uppercase;
+    color: #f9f9f9;
+    font-weight: 400;
+    font-size: 5em;
+    text-align: center;
+    margin: 5px;
+    font-family: 'TT Norms Pro', sans-serif;
+    scroll-behavior: smooth;
+  }
+
+  span {
+    font-weight: 600;
+    color: #65ffd1;
+    font-family: 'TT Norms Pro', sans-serif;
+    scroll-behavior: smooth;
+  }
+
+  h3 {
+    color: #ffffff;
+    font-weight: 300;
+    font-size: 2em;
+    text-align: center;
+  }
 `;
