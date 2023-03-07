85eb7bbb47bab55c8bd4142e365e3e32ee0268cb
Willian <willian.alves.b15@gmail.com>
2023-03-07 20:12:56 -0300

    styles(About): position of items

--- src/components/about/about.style.ts
+++ src/components/about/about.style.ts
@@ -3,11 +3,42 @@ import styled from "styled-components";
 
 
 export const About = styled.section`
+  padding: 100px;
 
+  h4 {
+    text-transform: initial;
+    color: #ffffff;
+    font-weight: 300;
+    font-size: 25px;
+    text-align: center;
+    letter-spacing: -0.02em;
+  }
+
+  h2 {
+    text-transform: uppercase;
+    color: #65ffd1;
+    font-weight: 600;
+    font-size: 45px;
+    text-align: center;
+    letter-spacing: -0.02em;
+  }
+
+  p {
+    color: #ffffff;
+    font-family: 'TT Norms Pro', monospace;
+    font-weight: 400;
+    font-size: 23px;
+    text-align: left;
+    margin-bottom: 15px;
+  }
 `;
 
 
 export const Heading = styled.div`
+  width: 100%;
 `;
 
-export const ContainerText = styled.div``;
+export const ContainerText = styled.div`
+  padding: 0 10px;
+  margin-top: 15px;
+`;
