0dcd20659b8651283c3127a313260a271b23bf56
Willian <willian.alves.b15@gmail.com>
2023-03-08 18:39:16 -0300

    style(contact): style contact

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -5,12 +5,14 @@ import * as C from "./contact.style";
 import linkedin from "../../assets/linkedin.svg";
 import email from "../../assets/email.svg";
 import github from "../../assets/github.svg";
+import whatsapp from "../../assets/whatsapp.svg";
+
 
 export default function Contact() {
   return(
     <C.Contact>
       <hr></hr>
-      <div>
+      <div className="heading">
         <h4>meus</h4>
         <h2>CONTATOS</h2>
       </div>
@@ -30,7 +32,13 @@ export default function Contact() {
             <Image src={email} alt="icone do email" width={50} height={50}/>
           </Link>
         </li>
+        <li>
+          <Link href="https://api.whatsapp.com/send?phone=5527997366918&text=" target="_blank">
+            <Image src={whatsapp} alt="icone do email" width={50} height={50}/>
+          </Link>
+        </li>
       </ul>
+      <p>Gostou <span>?</span> Bora trocar uma ideia <span>!</span></p>
       <hr></hr>
     </C.Contact>
   );
--- src/components/contact/contact.style.ts
+++ src/components/contact/contact.style.ts
@@ -1,4 +1,81 @@
 "use client"
 import styled from "styled-components";
 
-export const Contact = styled.section``;
+export const Contact = styled.section`
+  padding: 100px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  
+  .heading {
+    margin-top: 20px;
+  }
+
+  ul {
+    list-style: none;
+    display: flex;
+    justify-content: space-around;
+    margin-top: 50px;
+    width: 270px;
+  }
+
+  li {
+    transition: 0.3s;
+  }
+
+  li:hover {
+    margin-top: -5px;
+  }
+
+  h4 {
+    font-family: 'TT Norms Pro',sans-serif;
+    text-transform: initial;
+    color: #ffffff;
+    font-weight: 10;
+    font-size: 25px;
+    text-align: center;
+    -webkit-letter-spacing: -0.02em;
+    -moz-letter-spacing: -0.02em;
+    -ms-letter-spacing: -0.02em;
+    letter-spacing: -0.02em;
+  }
+
+  h2 {
+    text-transform: uppercase;
+    color: #65ffd1;
+    font-weight: 600;
+    font-size: 45px;
+    text-align: center;
+    -webkit-letter-spacing: -0.02em;
+    -moz-letter-spacing: -0.02em;
+    -ms-letter-spacing: -0.02em;
+    letter-spacing: -0.02em;
+    font-family: 'TT Norms Pro',sans-serif;
+  }
+
+  hr {
+    margin-top: 40px;
+    width: 60%;
+    border-radius: 50px;
+    border: none;
+    height: 2px;
+    margin-bottom: 10px;
+    background-image: linear-gradient(to right, transparent, #65ffd1, transparent);
+  }
+
+  p {
+    font-family: 'TT Norms Pro',sans-serif;
+    text-transform: initial;
+    color: #ffffff;
+    font-size: 20px;
+    text-align: center;
+    -webkit-letter-spacing: -0.02em;
+    -moz-letter-spacing: -0.02em;
+    -ms-letter-spacing: -0.02em;
+    letter-spacing: -0.02em;
+  }
+
+  span {
+    color: #65ffd1;
+  }
+`;
