af68b405c9477b349392d0b683a9c7d82f431f34
willianbatist <willian.alves.b15@gmail.com>
2025-12-02 17:06:47 -0300

    feat: update about

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -3,7 +3,7 @@ import * as A from "./about.style";
 
 
 export default function About() {
-  return(
+  return (
     <A.About id="sobre-mim">
       <A.Heading>
         <h4>Quem sou eu?</h4>
@@ -11,13 +11,23 @@ export default function About() {
       </A.Heading>
       <A.ContainerText>
         <p>
-        Meu nome é Willian Alves Batista, sou desenvolvedor Web Full Stack, formado pela Trybe, onde aprendi todas as tecnologias necessárias para desenvolver um site completo. Também sou graduado em Arquivologia, o que me levou a conhecer a Ciência da Informação e me deixou bastante intrigado a querer aprender programação.
+          Sou Willian Alves Batista, desenvolvedor Full Stack com dois anos de experiência prática,
+          formado pela Trybe, onde adquiri conhecimentos técnicos abrangentes para desenvolvimento
+          web completo. Minha trajetória inclui graduação em Arquivologia, experiência que me
+          introduziu à Ciência da Informação e despertou meu interesse pela programação.
         </p>
         <p>
-        Minha paixão pela tecnologia começou quando ganhei meu primeiro computador na adolescência, ver a interface e como ela reagia com a minha interação, me deixou fascinado por tecnologia. Hoje dedico a aprender sobre novas tecnologias e aplicá-las em meu trabalho. Acredito que a tecnologia é uma das principais ferramentas para o progresso humano e estou sempre ansioso para ver o que ela trará no futuro.
+          Minha paixão pela tecnologia começou quando ganhei meu primeiro computador na
+          adolescência, ver a interface e como ela reagia com a minha interação, me deixou fascinado
+          por tecnologia. Hoje dedico a aprender sobre novas tecnologias e aplicá-las em meu
+          trabalho. Acredito que a tecnologia é uma das principais ferramentas para o progresso
+          humano e estou sempre ansioso para ver o que ela trará no futuro.
         </p>
         <p>
-        Tenho conhecimento e experiência em JavaScript, TypeScript, Jest, Docker, Node.js, Nest.js, Express, React, Next.js, MySQL, MongoDB, Arquitetura Rest, Restful, programação orientada a objetos, teste unitário e teste automatizado. Busco sempre novas tecnologias e novas linguagens, no momento estou dando os primeiros passos em Python.
+          Tenho conhecimento e experiência em JavaScript, TypeScript, Jest, Docker, Node.js,
+          Nest.js, Express, React, Next.js, MySQL, MongoDB, Arquitetura Rest, Restful, programação
+          orientada a objetos, teste unitário e teste automatizado. Busco sempre novas tecnologias e
+          novas linguagens, no momento estou dando os primeiros passos em Python.
         </p>
       </A.ContainerText>
     </A.About>
